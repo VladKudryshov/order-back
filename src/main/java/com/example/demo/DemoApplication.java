de28f0d9ac30e4094f4221ef0ec9d92279551157
kudr9tov <uladzislau.kudrashou@instinctools.ru>
2018-10-04 14:34:15 +0300

    udpate postgresql

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,11 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		URI dbUri = null;
+		try {
+			dbUri = new URI(System.getenv("DATABASE_URL"));
+			String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+			System.out.println(dbUrl);
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
 	}
 }
