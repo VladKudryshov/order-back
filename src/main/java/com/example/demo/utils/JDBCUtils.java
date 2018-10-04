64c952fe9d4ebaa9a2cea3bd4e7a756df32a55e1
kudr9tov <uladzislau.kudrashou@instinctools.ru>
2018-10-04 14:40:29 +0300

    udpate postgresql

--- src/main/java/com/example/demo/utils/JDBCUtils.java
+++ src/main/java/com/example/demo/utils/JDBCUtils.java
@@ -2,19 +2,22 @@ package com.example.demo.utils;
 
 import org.postgresql.ds.PGConnectionPoolDataSource;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class JDBCUtils {
-    private static PGConnectionPoolDataSource poolDataSource = null;
+    public static Connection getAccountsConnection() throws SQLException, URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+        PGConnectionPoolDataSource poolDataSource = new PGConnectionPoolDataSource();
 
-    public static Connection getAccountsConnection() throws SQLException {
-        if (poolDataSource == null) {
-            poolDataSource = new PGConnectionPoolDataSource();
-            poolDataSource.setUrl("postgres://xwfvhucamvvpzw:fe71fe6cb56913f92736a5310aa2e43e1d40c598f027abb81ffecac8ed899fa6@ec2-54-225-68-133.compute-1.amazonaws.com:5432/de7e9t23pbe9gs");
-            poolDataSource.setUser("xwfvhucamvvpzw");
-            poolDataSource.setPassword("fe71fe6cb56913f92736a5310aa2e43e1d40c598f027abb81ffecac8ed899fa6");
+        if (dbUri.getUserInfo() != null) {
+            poolDataSource.setUser(dbUri.getUserInfo().split(":")[0]);
+            poolDataSource.setPassword(dbUri.getUserInfo().split(":")[1]);
         }
+        poolDataSource.setUrl(dbUrl);
         return poolDataSource.getConnection();
     }
 }
